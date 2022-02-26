99cac572ece561a73c62b649ad10a2d1e29e743e
phucle-gin <hoangphuc2832000@gmail.com>
2022-02-26 15:46:29 +1100

    Updates

---
+++ precache-manifest.f49962ace985d5e62dc647f5d2f2051b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d674231d76615d6ac40f",
+    "url": "/react-expense-tracker/static/css/main.56480343.chunk.css"
+  },
+  {
+    "revision": "d674231d76615d6ac40f",
+    "url": "/react-expense-tracker/static/js/main.d674231d.chunk.js"
+  },
+  {
+    "revision": "935b0251a626769e024b",
+    "url": "/react-expense-tracker/static/js/1.935b0251.chunk.js"
+  },
+  {
+    "revision": "45de306f3f336216fdf0",
+    "url": "/react-expense-tracker/static/js/2.45de306f.chunk.js"
+  },
+  {
+    "revision": "56a2c86a257f87cabb14",
+    "url": "/react-expense-tracker/static/js/runtime~main.56a2c86a.js"
+  },
+  {
+    "revision": "4980265def8c8f07fa83f8aec362b112",
+    "url": "/react-expense-tracker/index.html"
+  }
+];
