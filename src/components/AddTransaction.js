05a66128af5c8e41f4d96f02bebc0be4812156d3
phucle-gin <hoangphuc2832000@gmail.com>
2022-02-26 16:59:35 +1100

    Fixing AddTransaction css and a hook to update state from form input

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form id="form">
         <div class="form-control">
           <label for="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div class="form-control">
           <label for="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button class="btn">Add transaction</button>
       </form>
